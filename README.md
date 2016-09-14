494a56136ff9fb37d572dcd904b9150589c56c3e
Rodrigo Fuenzalida <rfuenzalidac87@gmail.com>
2016-09-14 11:19:56 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# Finciero JS Zipkin
+
+[![NPM](https://nodei.co/npm/tracer-js.png)](https://nodei.co/npm/tracer-js/)
+
+[![npm version](https://badge.fury.io/js/tracer-js.svg)](https://badge.fury.io/js/tracer-js)
+
+tracer-js is an abstraction of [zipkin-js](https://github.com/openzipkin/zipkin-js)
+This library let us start a new `zipkin tracer` or continue a `child trace`.
+
+# Config zipking and kafka.
+
+To use this library you need setup `KAFKA_SERVICE_URL` in your environment variables.
+Also, you can use [this](https://github.com/openzipkin/docker-zipkin) to run kafka and zipkin with docker.
+
+# Usage
+
+The basic usage is as follow:
+
+```js
+const { TracerJS } = require('tracerjs')
+
+const tracer = new TracerJS()
+tracer.config({
+  name: 'service-name',
+  kind: 'service-kind',
+  logger: someLogger(),
+})
+```
+
+then you can start a new trace with:
+
+```js
+tracer.start()
+```
+
+Or you can pass and object with an old tracer:
+
+```js
+const oldTracerData = {
+  traceId: 'some-trace-id',
+  parentId: 'some-parent-id',
+  spanId: 'some-span-id',
+  sampled: true,
+}
+tracer.start(oldTracerData)
+```
+
+And this will continue the trace given in the method parameters.
+
+To finish a trace record, just use:
+
+```js
+tracer.finish()
+```
+
+This will stop the current trace record. If an error happen in your program, you can pass as argument the error and the `finish` method will trace the error, this push the error data to kafka and zipkin.
+
+```js
+const err = new Error('awesome error')
+tracer.finish(err)
+```
---
+++ tracer.js
@@ -0,0 +1,155 @@
+const {
+  Annotation,
+  BatchRecorder,
+  ExplicitContext,
+  option: { Some, None },
+  Tracer,
+  TraceId,
+} = require('zipkin')
+const lodash = require('lodash')
+const { KafkaLogger } = require('zipkin-transport-kafka')
+const { NOOPLogger } = require('finciero-logger')
+const ctxImpl = new ExplicitContext()
+
+// helpers
+const containsRequiredMeta = (meta) => {
+  if (typeof meta !== 'undefined') {
+    return meta.traceId !== undefined && meta.spanId !== undefined
+  }
+  return false
+}
+
+const readVal = (val) => {
+  if (val != null) {
+    return new Some(val)
+  }
+  return None
+}
+
+let instance = null
+
+class TracerJS {
+  constructor () {
+    this.tracer = new Tracer({
+      recorder: new BatchRecorder({
+        logger: new KafkaLogger({
+          clientOpts: {
+            connectionString: process.env.KAFKA_SERVICE_URL,
+          },
+        }),
+      }),
+      ctxImpl,
+    })
+    this.serviceName = undefined
+    this.serviceKind = undefined
+    this.logger = undefined
+    this.port = 0
+    this.currentId = undefined
+    this.extra = undefined
+    this.nextTrace = {
+      traceId: undefined,
+      spanId: undefined,
+      parentId: undefined,
+      sampled: undefined,
+    }
+
+    if (!instance) {
+      instance = this
+    }
+
+    return instance
+  }
+
+  config ({ name = 'unknown', kind = 'server', logger = NOOPLogger }) {
+    this.serviceName = name
+    this.serviceKind = kind
+    this.logger = logger
+  }
+
+  setExtraData (extra) {
+    if (typeof extra === 'undefined') {
+      throw new Error('Extra data should be an object.')
+    }
+    this.extra = extra
+  }
+
+  setPort (port) {
+    this.port = port
+  }
+
+  start (meta) {
+    this.logger.info('tracer:start')
+
+    if (containsRequiredMeta(meta)) {
+      // child process
+      const { traceId, parentId, spanId, sampled } = meta
+      const oldSpanId = readVal(spanId)
+      oldSpanId.ifPresent(sid => {
+        const id = new TraceId({
+          traceId: readVal(traceId),
+          parentId: readVal(parentId),
+          spanId: sid,
+          sampled: readVal(sampled),
+          flags: 0,
+        })
+        this.tracer.setId(id)
+      })
+    } else {
+      this.tracer.setId(
+        this.tracer.createRootId()
+      )
+    }
+    this.currentId = this.tracer.id
+
+    const childId = this.tracer.createChildId()
+    const { traceId, spanId, parentId, sampled } = childId
+    Object.assign(this.nextTrace, {
+      traceId,
+      spanId,
+      parentId,
+      sampled,
+    })
+
+    this.tracer.recordServiceName(this.serviceName)
+    this.tracer.recordRpc('Scraper')
+    if (this.extra) {
+      Object.keys(this.extra).forEach((prop) => {
+        const baggageKeys = ['user-id', 'request-id', 'task-id']
+        const isBaggageKey = k => baggageKeys.includes(lodash.snakeCase(k))
+        const normalizeKey = k => (
+          isBaggageKey(k) ? lodash.snakeCase(k) : `${this.serviceName}.${k}`
+        )
+
+        this.tracer.recordBinary(normalizeKey(prop), this.extra[prop])
+      })
+    }
+    this.tracer.recordBinary('Local Component', this.serviceName)
+    this.tracer.recordBinary('span.kind', this.serviceKind)
+    this.tracer.recordAnnotation(new Annotation.ServerRecv())
+    this.tracer.recordAnnotation(new Annotation.LocalAddr({ port: this.port }))
+  }
+
+  finish (err) {
+    if (err) {
+      this.tracer.scoped(() => {
+        this.tracer.setId(this.currentId)
+        this.tracer.recordBinary(`${this.serviceName}.status`, 'fail')
+        this.tracer.recordBinary('error', err.toString())
+        this.tracer.recordAnnotation(new Annotation.ServerSend())
+      })
+    } else {
+      this.tracer.scoped(() => {
+        this.tracer.setId(this.currentId)
+        this.tracer.recordBinary(`${this.serviceName}.status`, 'ok')
+        this.tracer.recordAnnotation(new Annotation.ServerSend())
+      })
+    }
+
+    this.logger.info('tracer:finish')
+  }
+}
+
+exports = module.exports = {
+  default: TracerJS,
+  TracerJS,
+}
